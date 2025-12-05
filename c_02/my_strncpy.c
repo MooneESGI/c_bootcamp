f5c4cc16a37c9d4a7a22e0c129aef300f35f2d47
Milo <m.hugonenqdelabonnef@myskolae.fr>
2025-12-05 11:54:27 +0100

    Exercice 02 fonctionel

---
+++ c_02/my_strncpy.c
@@ -0,0 +1,24 @@
+#include "my_strncpy.h"
+
+char *my_strncpy(char *dest, const char *src, size_t n)
+{
+    size_t i = 0;
+
+    if (!dest || !src)
+        return dest; // sécurité minimale
+
+    while (i < n && src[i] != '\0')
+    {
+        dest[i] = src[i];
+        i++;
+    }
+
+    // Remplir le reste avec '\0' si src < n
+    while (i < n)
+    {
+        dest[i] = '\0';
+        i++;
+    }
+
+    return dest;
+}
---
+++ c_02/my_strncpy.h
@@ -0,0 +1,8 @@
+#ifndef MY_STRNCPY_H
+#define MY_STRNCPY_H
+
+#include <stddef.h>
+
+char *my_strncpy(char *dest, const char *src, size_t n);
+
+#endif
